92746ffc25a048382c59e61b9f334de56b048033
biaov <biaov@qq.com>
2024-05-07 17:45:09 +0800

    更新代码

--- scripts/tag.js
+++ scripts/tag.js
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
-import pkg from '../package.json'
+import pkg from '../package.json' assert { type: 'json' }
 
 execSync(`git tag v${pkg.version}`)
 execSync(`git push origin v${pkg.version}`)
