7f6ccac980a3d89a45af69247e1d9140d98f5a22
biaov <biaov@qq.com>
2024-05-07 17:57:47 +0800

    更新代码

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,10 @@
-import { existsSync, mkdirSync, cpSync } from 'fs'
+import { existsSync, mkdirSync, cpSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 
 const rootDir = resolve(import.meta.dirname, '../')
 !existsSync('dist') && mkdirSync('dist')
-;['package.json', 'package-lock.json', 'README.md', 'LICENSE', 'index.js'].forEach(name => {
+;['package.json', 'README.md', 'LICENSE', 'index.js'].forEach(name => {
   cpSync(resolve(rootDir, name), resolve(rootDir, 'dist', name))
 })
+
+writeFileSync(resolve(rootDir, 'dist/demo.js'), `console.log('npm-demo-demo')`)
